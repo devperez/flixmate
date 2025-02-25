f4fc5e84bc82e7e60a6088ee471a3c9e884a4fd4
David <advitameternam@gmail.com>
2025-02-25 11:42:52 +0100

    Improved list display

--- resources/js/Pages/Show.vue
+++ resources/js/Pages/Show.vue
@@ -11,22 +11,25 @@
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="flex justify-end">
-                    <button @click="openShareModal(index)" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    <button @click="openShareModal(index)"
+                        class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
                         Partager la liste
                     </button>
                 </div>
                 <div v-if="list">
                     <h1 class="text-2xl font-bold mb-4">{{ list.name }}</h1>
-                    <div v-if="list.items && list.items.length > 0" class="grid grid-cols-2 md:grid-cols-4 gap-4">
-                        <div v-for="item in list.items" :key="item.id" class="text-center">
+                    <div v-if="list.items && list.items.length > 0"
+                        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        <div v-for="item in list.items" :key="item.id"
+                            class="border rounded-lg shadow-md p-4 text-center">
                             <img v-if="item.movie" :src="'https://image.tmdb.org/t/p/w500' + item.movie.poster_path"
-                                alt="Affiche du film" class="w-32 h-48 mx-auto rounded-lg shadow">
-                            <p class="mt-2">{{ item.movie?.title }}</p>
+                                alt="Affiche du film" class="w-full h-auto rounded-lg shadow mb-4">
+                            <p class="text-lg font-semibold">{{ item.movie?.title }}</p>
                         </div>
                     </div>
-
                     <p v-else class="text-gray-500">Cette liste ne contient aucun film.</p>
                 </div>
+
                 <p v-else class="text-red-500">Chargement de la liste...</p>
             </div>
         </div>
@@ -45,8 +48,9 @@ import { ref, onMounted } from 'vue';
 const showShareModal = ref(false);
 const activeConnections = ref([]);
 const props = defineProps({
-    list: { Object,
-    required: true
+    list: {
+        Object,
+        required: true
     }
 });
 
