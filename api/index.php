cf39bcdf29a0eccf9aaeda31963bc5da3c862685
David <advitameternam@gmail.com>
2025-03-26 09:34:21 +0100

    some vercel config changes

--- api/index.php
+++ api/index.php
@@ -2,17 +2,24 @@
 
 use Illuminate\Http\Request;
 
+// Charger l'autoloader de Composer
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../bootstrap/app.php';
 
+// Créer l'application Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// Créer le noyau de l'application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Request::capture()
-);
+// Capturer la requête
+$request = Request::capture();
 
+// Gérer la requête
+$response = $kernel->handle($request);
+
+// Envoyer la réponse
 $response->send();
 
+// Terminer le kernel
 $kernel->terminate($request, $response);
+
