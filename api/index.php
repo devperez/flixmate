e7e7d22ceb0b1d6927aed7f7d2943d57f0130d0c
David <advitameternam@gmail.com>
2025-03-31 10:02:23 +0200

    changes for vercel

--- api/index.php
+++ api/index.php
@@ -1,25 +1,2 @@
 <?php
-
-use Illuminate\Http\Request;
-
-// Charger l'autoloader de Composer
-require __DIR__ . '/../vendor/autoload.php';
-
-// Créer l'application Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Créer le noyau de l'application
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-// Capturer la requête
-$request = Request::capture();
-
-// Gérer la requête
-$response = $kernel->handle($request);
-
-// Envoyer la réponse
-$response->send();
-
-// Terminer le kernel
-$kernel->terminate($request, $response);
-
+require __DIR__ . '/../public/index.php';
