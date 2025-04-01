52953ec019e84167cdfdd037a356d1dfd47d16f4
David <advitameternam@gmail.com>
2025-04-01 12:41:11 +0200

    added db config to the database.php file

--- config/database.php
+++ config/database.php
@@ -84,12 +84,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('postgres://neondb_owner:npg_sOoEfje9ViQ7@ep-solitary-surf-a2tol9c0-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'),
+            'host' => env('ep-solitary-surf-a2tol9c0-pooler.eu-central-1.aws.neon.tech'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('neondb'),
+            'username' => env('neondb_owner'),
+            'password' => env('npg_sOoEfje9ViQ7'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
